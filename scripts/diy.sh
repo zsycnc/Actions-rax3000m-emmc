b26548d4978ccc8b199856ceb58258da5276e90a
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-01-24 01:17:33 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -2,3 +2,7 @@
 
 #更改默认地址为192.168.6.1
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#添加helloworld
+echo 'src-git helloworld https://github.com/fw876/helloworld;master' >> feeds.conf.default
+#添加easymesh
+echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh;master' >> feeds.conf.default
