783a030dc663222e1beb1084c02c42f2060c4176
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-01-24 01:21:15 +0800

    Create diy1.sh

---
+++ scripts/diy1.sh
@@ -0,0 +1,4 @@
+#添加helloworld
+echo 'src-git helloworld https://github.com/fw876/helloworld;master' >> feeds.conf.default
+#添加easymesh
+echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh;master' >> feeds.conf.default
