2a1e12d84455772eb8fea2b5021237fb90c8c45c
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-01-24 14:21:42 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -4,3 +4,9 @@ echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld;master' >> f
 echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh;master' >> feeds.conf.default
 #添加luci-app-adguardhome
 echo 'src-git luci-app-easymesh https://github.com/rufengsuixing/luci-app-adguardhome.git;master' >> feeds.conf.default
+# istore
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+#docker
+echo 'src-git luci-app-docker https://github.com/brokeld/luci-app-docker.git;master' >> feeds.conf.default
