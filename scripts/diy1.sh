a8d7c8ba3f08542bb18ee174ab6d6d30b7d912bd
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-01-24 12:49:31 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -1,4 +1,6 @@
 #添加helloworld
-echo 'src-git helloworld https://github.com/fw876/helloworld;master' >> feeds.conf.default
+echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld;master' >> feeds.conf.default
 #添加easymesh
 echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh;master' >> feeds.conf.default
+#添加luci-app-adguardhome
+echo 'src-git luci-app-easymesh https://github.com/rufengsuixing/luci-app-adguardhome.git;master' >> feeds.conf.default
