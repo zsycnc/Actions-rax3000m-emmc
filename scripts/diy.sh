1e97466fcd873511aec84c07d87ad6897e9288d2
lbpcvv <lbpcvv@gmail.com>
2024-05-13 09:37:18 +0800

    Update diy.sh

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#更改默认地址为192.168.6.1
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+#更改默认地址为192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
