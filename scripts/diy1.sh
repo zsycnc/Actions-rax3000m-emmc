e6b842534c7e26f7796efc8aec5be7428ff47e87
lbpcvv <lbpcvv@gmail.com>
2024-05-14 17:56:01 +0800

    Update diy1.sh

--- scripts/diy1.sh
+++ scripts/diy1.sh
@@ -1,12 +1,9 @@
 #添加helloworld
 echo 'src-git luci-app-ssr-plus https://github.com/fw876/helloworld.git' >> feeds.conf.default
 #添加easymesh
-echo 'src-git luci-app-easymesh https://github.com/shuishihan/easymesh.git' >> feeds.conf.default
-#添加luci-app-adguardhome
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
 # istore
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 #echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 #docker
-echo 'src-git luci-app-docker https://github.com/brokeld/luci-app-docker.git' >> feeds.conf.default
+
